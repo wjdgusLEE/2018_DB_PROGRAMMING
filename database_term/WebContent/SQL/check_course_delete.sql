c3d97ea56ef2889bc171a0ff935dc2b13ef78952
jiun\jiunoh <juneoh227@gmail.com>
2018-06-10 22:19:56 +0900

    sql for course deleting

---
+++ database_term/WebContent/SQL/check_course_delete.sql
@@ -0,0 +1,38 @@
+
+CREATE OR REPLACE FUNCTION checkDelete(
+p_cid IN VARCHAR2,
+p_cid_no IN NUMBER
+)
+RETURN NUMBER
+
+IS
+v_count NUMBER;
+result NUMBER;
+
+BEGIN
+result := 0;
+
+IF (p_cid_no = 1) THEN
+  DELETE teach
+  WHERE c_id = p_cid;
+
+  DELETE course
+  WHERE c_id = p_cid;
+ELSE
+  DELETE teach
+  WHERE c_id = p_cid AND c_id_no=p_cid_no;
+
+  DELETE course
+  WHERE c_id = p_cid AND c_id_no=p_cid_no;
+END IF;
+
+result := 1;
+RETURN result;
+
+EXCEPTION
+WHEN OTHERS THEN
+  ROLLBACK;
+  result := SQLCODE;
+  RETURN result;
+END;
+/
